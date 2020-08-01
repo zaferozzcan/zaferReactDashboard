e07bacc1ec16ab820a12634c2273af05cab124af
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-01 02:11:05 -0400

    Add

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,47 @@
+// import React, { Component } from "react";
+// import ProjectList from "../projects/ProjectList";
+// import Notifications from "./Notifications";
+// import { connect } from "react-redux";
+// // import { firestoreConnect } from "react-redux-firebase";
+// // import { compose } from "redux";
+
+// class Dashboard extends Component {
+//   render() {
+//     // console.log(this.props);
+//     const { projects } = this.props;
+
+//     return (
+//       <div className="dashboard container">
+//         <div className="row">
+//           <div className="col s12 m6">
+//             <ProjectList projects={projects} />
+//           </div>
+//           <div className="col s12 m5 offset-m1">
+//             <Notifications />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     projects: state.project.projects
+//   };
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   null
+// )(Dashboard);
+
 import React, { Component } from "react";
 import ProjectList from "../projects/ProjectList";
 import Notifications from "./Notifications";
 import { connect } from "react-redux";
-// import { firestoreConnect } from "react-redux-firebase";
-// import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends Component {
   render() {
@@ -26,12 +64,13 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
+  // console.log(state);
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   };
 };
 
-export default connect(
-  mapStateToProps,
-  null
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
 )(Dashboard);
