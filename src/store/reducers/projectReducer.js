65256ca393af22da0c270936675ec545934ca3cd
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-10 15:41:59 -0400

    add

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -8,9 +8,10 @@ const initState = {
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
-    case "CREATE_PROJECT_SUCCESS":
-      console.log("create project success");
+    case "CREATE_PROJECT":
+      console.log("create project success", action.project);
       return state;
+
     case "CREATE_PROJECT_ERROR":
       console.log("create project error");
       return state;
