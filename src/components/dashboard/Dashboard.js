47771ea4d2a23eb2b0f94a963ab35523fd0c193b
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-18 22:01:16 -0400

    Fix bugs in the createProject components and Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,38 +1,3 @@
-// import React, { Component } from "react";
-// import ProjectList from "../projects/ProjectList";
-// import Notifications from "./Notifications";
-// import { connect } from "react-redux";
-// // import { firestoreConnect } from "react-redux-firebase";
-// // import { compose } from "redux";
-
-// class Dashboard extends Component {
-//   render() {
-//     // console.log(this.props);
-//     const { projects } = this.props;
-
-//     return (
-//       <div className="dashboard container">
-//         <div className="row">
-//           <div className="col s12 m6">
-//             <ProjectList projects={projects} />
-//           </div>
-//           <div className="col s12 m5 offset-m1">
-//             <Notifications />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     projects: state.project.projects
-//   };
-// };
-
-// export default connect(mapStateToProps, null)(Dashboard);
-
 import React, { Component } from "react";
 import ProjectList from "../projects/ProjectList";
 import Notifications from "./Notifications";
@@ -61,7 +26,7 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
+  // console.log(state);
   return {
     projects: state.firestore.ordered.projects || state.project.projects
   };
