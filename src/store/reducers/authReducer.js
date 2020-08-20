6c0d0ffa778d5ce498d78ab6ca42efdba82f9c80
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-20 01:55:51 -0400

    Auth action

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,23 @@
-const initState = {};
+const initState = {
+  authError: null
+};
 
-function authReducer(state = initState, action) {
-  return state;
-}
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("login error");
+      return {
+        ...state,
+        authError: "Login failed"
+      };
+    case "LOGIN_SUCCESS":
+      console.log("login success");
+      return {
+        authError: null
+      };
+    default:
+      return state;
+  }
+};
 
 export default authReducer;
