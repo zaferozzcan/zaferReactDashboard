92cd2aa7548d2fcd5898f17dda770aa14ed2606d
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-26 11:03:09 -0400

    Edit sigedInLinks

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,22 +1,55 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// function SignedInLinks() {
+//   return (
+//     <ul className="right hide-on-med-and-down">
+//       <li>
+//         <NavLink to="/create"> NewProject </NavLink>{" "}
+//       </li>
+//       <li>
+//         <NavLink to="/"> LogOut </NavLink>{" "}
+//       </li>
+//       <li>
+//         <NavLink to="/" className="btn btn-floating lighten-1 yellow">
+//           ZO
+//         </NavLink>{" "}
+//       </li>
+//     </ul>
+//   );
+// }
+
+// export default SignedInLinks;
+
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
-function SignedInLinks() {
+const SignedInLinks = (props) => {
   return (
-    <ul className="right hide-on-med-and-down">
-      <li>
-        <NavLink to="/create"> NewProject </NavLink>{" "}
-      </li>
-      <li>
-        <NavLink to="/"> LogOut </NavLink>{" "}
-      </li>
-      <li>
-        <NavLink to="/" className="btn btn-floating lighten-1 yellow">
-          ZO
-        </NavLink>{" "}
-      </li>
-    </ul>
+    <div>
+      <ul className="right hide-on-med-and-down">
+        <li>
+          <NavLink to="/create">New Project</NavLink>
+        </li>
+        <li>
+          <a onClick={props.signOut}>Log Out</a>
+        </li>
+        <li>
+          <NavLink to="/" className="btn btn-floating pink lighten-1">
+            NN
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
-}
+};
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
