5382e69fb69a10a4081b71f7b54e91960577c595
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-26 11:11:30 -0400

    Edit navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,3 +1,33 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import SignedInLinks from "./SignedInLinks";
+// import SignedOutLinks from "./SignedOutLink";
+// import { connect } from "react-redux";
+
+// const Navbar = (props) => {
+//   const { auth } = props;
+//   return (
+//     <nav className="nav-wrapper light-blue darken-3">
+//       <div className="container">
+//         <Link to="/" className="brand-logo">
+//           Zafer Ozcan
+//         </Link>
+//         <SignedInLinks />
+//         <SignedOutLinks />
+//       </div>
+//     </nav>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   // console.log(state);
+//   return {
+//     auth: state.firebase.auth
+//   };
+// };
+
+// export default connect(mapStateToProps)(Navbar);
+
 import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
@@ -6,11 +36,12 @@ import { connect } from "react-redux";
 
 const Navbar = (props) => {
   const { auth } = props;
+  console.log(auth);
   return (
-    <nav className="nav-wrapper light-blue darken-2">
+    <nav className="nav-wrapper light-blue darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          Zafer Ozcan
+          MarioPlan
         </Link>
         <SignedInLinks />
         <SignedOutLinks />
