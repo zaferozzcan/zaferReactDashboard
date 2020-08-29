ce7d5bcc05d49137928368ea8f55c319ea189761
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-29 14:50:35 -0400

    Auth reducer

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,33 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import SignedInLinks from "./SignedInLinks";
-// import SignedOutLinks from "./SignedOutLink";
-// import { connect } from "react-redux";
-
-// const Navbar = (props) => {
-//   const { auth } = props;
-//   return (
-//     <nav className="nav-wrapper light-blue darken-3">
-//       <div className="container">
-//         <Link to="/" className="brand-logo">
-//           Zafer Ozcan
-//         </Link>
-//         <SignedInLinks />
-//         <SignedOutLinks />
-//       </div>
-//     </nav>
-//   );
-// };
-
-// const mapStateToProps = (state) => {
-//   // console.log(state);
-//   return {
-//     auth: state.firebase.auth
-//   };
-// };
-
-// export default connect(mapStateToProps)(Navbar);
-
 import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
@@ -41,7 +11,7 @@ const Navbar = (props) => {
     <nav className="nav-wrapper light-blue darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MarioPlan
+          Zafer Ozcan
         </Link>
         <SignedInLinks />
         <SignedOutLinks />
