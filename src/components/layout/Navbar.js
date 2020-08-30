9a15302f0f40b91f9bc312f1cd5b91a960e0fa80
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-30 14:27:11 -0400

    Navbar wont work

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
-import SignedOutLinks from "./SignedOutLink";
+import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const Navbar = (props) => {
   const { auth } = props;
-  console.log(auth);
+  // console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+
   return (
-    <nav className="nav-wrapper light-blue darken-3">
+    <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          Zafer Ozcan
+          MarioPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
