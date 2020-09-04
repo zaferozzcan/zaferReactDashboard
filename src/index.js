f02f336cb38816b173a1fe66746d41bc1c75960f
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-09-03 23:01:29 -0400

    Index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import "./index.css";
 import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
 import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
@@ -13,14 +15,16 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reactReduxFirebase(fbConfig), // redux binding for firebase
+    reactReduxFirebase(fbConfig, { attachAuthIsReady: true }), // redux binding for firebase
     reduxFirestore(fbConfig) // redux bindings for firestore
   )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+});
